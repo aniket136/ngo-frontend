dd772d1be55f0dd91b92ee22008ce7d8914747c7
Aniket8929 <aniketkumarrai271@gmail.com>
2025-04-10 23:48:49 +0530

    Added homepage for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
-// // Replace 'ngo' with your repo name if different
+// // https://vite.dev/config/
 // export default defineConfig({
-//   base: 'ngo-frontend',
 //   plugins: [react()],
 // })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/ngo-frontend/', // 👈 important for GitHub Pages
+  plugins: [react()],
+})
